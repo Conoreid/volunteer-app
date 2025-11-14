b3d38696ff54c507f6b215555cf7c2615e7ae8ec
BuildTools <93288260+SlimsyKhan7@users.noreply.github.com>
2025-11-14 13:58:29 +0000

    added pointer to map

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useRef, useCallback } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+
+const INITIAL_REGION = {
+  latitude: 55.861486896052135,
+  longitude: -4.2423500238778145,
+  latitudeDelta: 0.0002,
+  longitudeDelta: 0.0002,
+};
 
 export default function App() {
+  const mapRef = useRef<MapView>(null);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const snapPoints = ['1%', '35%', '60%'];
+
+  const handleMarkerPress = useCallback(() => {
+    bottomSheetRef.current?.snapToIndex(1);
+  }, []);
+
   return (
     <View className="h-full w-full flex-1">
       <MapView
-        initialRegion={{
-          latitude: 55.861486896052135,
-          longitude: -4.2423500238778145,
-          latitudeDelta: 1,
-          longitudeDelta: 1,
-        }}
+        initialRegion={INITIAL_REGION}
+        provider={PROVIDER_GOOGLE}
         style={{ height: '100%', width: '100%' }}
-      />
+        showsUserLocation
+        showsMyLocationButton
+        ref={mapRef}>
+        <Marker
+          onPress={handleMarkerPress}
+          coordinate={{ latitude: 55.861486896052135, longitude: -4.2423500238778145 }}
+        />
+      </MapView>
+      <BottomSheet ref={bottomSheetRef} index={0} snapPoints={snapPoints}>
+        <BottomSheetView>
+          <Text className="p-20">Marker Details Menu!</Text>
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   );
 }
