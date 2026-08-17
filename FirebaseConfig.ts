b16ace13f26b4ebccf0e9541d7bf9b80e7b4cbe5
SlimsyKhan7 <slimsykhan7@gmail.com>
2026-08-17 13:59:10 +0100

    updates firebase config to use new firebase project and api key

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -4,14 +4,23 @@ import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBZseBXAb4bDrK8bOqqj-BVSVz-CihK3Ic',
-  authDomain: 'volunteer-app-fe240.firebaseapp.com',
-  projectId: 'volunteer-app-fe240',
-  storageBucket: 'volunteer-app-fe240.firebasestorage.app',
-  messagingSenderId: '898562480771',
-  appId: '1:898562480771:web:88391c13a58c3f713159b6',
-  measurementId: 'G-QX8DREQWD1',
+
+  apiKey: "AIzaSyBLxXlJl4zT-slnoVnBVj9qfg1V5AB6A-I",
+
+  authDomain: "a4g-2026.firebaseapp.com",
+
+  projectId: "a4g-2026",
+
+  storageBucket: "a4g-2026.firebasestorage.app",
+
+  messagingSenderId: "303798777456",
+
+  appId: "1:303798777456:web:29f0366c34ec4e43bde6b5",
+
+  measurementId: "G-37MB828YFK"
+
 };
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
